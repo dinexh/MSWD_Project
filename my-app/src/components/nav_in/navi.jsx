a613fe89e358bbb578408b0f55a8165a9becfe81
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-11-05 14:10:19 +0530

    adjusted nav

--- my-app/src/components/nav_in/navi.jsx
+++ my-app/src/components/nav_in/navi.jsx
@@ -11,9 +11,17 @@ const Navi = () => {
         <div className="navi">
             <div className="navi-in">
                 <div className="navi-in-one">
-                    <h1>Sports Management System</h1>
+                    <h1> <span>Sports Management System</span></h1>
                 </div>
                 <div className="navi-in-two">
+                    <div className="navi-in-two-in">
+                        <h2>Live Score</h2>
+                        <h2>Event Management</h2>
+                        <h2>All Events</h2>
+                        <h2>Players Stats</h2>
+                    </div>
+                </div>
+                <div className="navi-in-three">
                     <button onClick={GoToLogin}>
                         Get Started
                     </button>
